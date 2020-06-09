215114b104a7346046272873357fe59347e1ca79
David Hernandez Azagury <davidhazagury@gmail.com>
2020-06-09 15:27:10 +0200

    Display task by clicking on it . DONE

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,8 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all # Returns an array with all the tasks.
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
